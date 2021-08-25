65d088d51ec34dc11e088f1891cae94aa4f79093
Uncharted <uncha@LAPTOP-1PJR3MM8>
2021-08-25 16:03:23 +0200

    Add project files.

---
+++ ConsoleAppDatabaseFistAirportH3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDatabaseFistAirportH3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (AirportDBEntities context = new AirportDBEntities())
+            {
+                Router router = new Router
+                {
+                    HomeIATACode = "CPH",
+                    DestinationIATACode = "LHR",
+                    RouteOwnerId = 3,
+
+                };
+
+                context.Routers.Add(router);
+                context.SaveChanges();
+
+                //Router route = context.Routers.FirstOrDefault(r => r.RouteId == 1);
+
+            }
+        }
+    }
+}
